70ef3a1e3bff05710394fee7a1d9111894b6c9fa
Vishal51-git <vishal.311gupta@gmail.com>
2025-03-27 21:09:59 +0530

    Add files via upload
    first commit

---
+++ next.config.ts
@@ -0,0 +1,12 @@
+module.exports = {
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          { key: 'Cache-Control', value: 'public, max-age=3600' },
+        ],
+      },
+    ];
+  },
+};
